c6f9cfb87b78478ae38af04e7bc9de5d0af880ec
DiogS5633 <diogo.mendoza@estudante.ifmt.edu.br>
2024-11-12 15:13:02 +0000

    0

--- ola.py
+++ ola.py
@@ -1,20 +1,31 @@
-print("Olá Mundo!")
+'''
+Exemplo de Graphics User Interface (GUI) com Python
+'''
 
-nome   = 'joca' #variavel global
-idade  = 0
-altura = 1.5 
-print(f'Olá {nome} Mundo!')
+from tkinter import Tk, Label, Entry, Button, messagebox
 
-def ola(nome): #variavel local
-    print(f'Olá {nome} Seja Bem Vindo Mundo!')
+def show_nome():
+    lbl_resultado.config(text =  lbl_resultado.cget('text') + txt_nome.get() + '\n')
+    messagebox.showinfo(
+            title='Seja Bem Vindo!',
+            message=f'Olá {txt_nome.get()}')
+    txt_nome.delete(0, len(txt_nome.get()))
 
+window = Tk()
+window.title('Minha Janela')
+window.config(padx=10, pady=10)
 
-ola('LUCA')
-ola('TUCAS')
+# Labels
+lbl_nome = Label(text='Nome:')
+lbl_nome.grid(row=0, column=0)
+lbl_resultado = Label(text='')
+lbl_resultado.grid(row=2, column=0, columnspan=3, sticky='W')
 
-ola(input('Digite seu nome: '))
+# Entries
+txt_nome = Entry(width=35)
+txt_nome.grid(row=0, column=1, columnspan=2, sticky='W')
+txt_nome.focus()
+btn_ok = Button(text="OK", width=20, command=show_nome)
+btn_ok.grid(row=1, column=0, columnspan=3)
 
-def num(idade):
-    print("Insira numero: ")
-
-num(input(idade))    
+window.mainloop()
